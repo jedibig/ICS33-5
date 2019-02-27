8c5399a1aa5632cfb9d4e0c82df201dbf21d5bba
Jedidiah Bowo <jbowo@personalmicrosoftsoftware.uci.edu>
2019-02-27 14:06:06 -0800

    modify update to have random velocity

--- floater.py
+++ floater.py
@@ -11,15 +11,15 @@ from random import random
 
 
 class Floater(Prey): 
-    radius = speed = 5
-    def __init__():
-        Prey.__init__(self,x,y,self.radius,self.radius,0,self.speed)
+    def __init__(self, x, y):
+        _image = PhotoImage(file='ufo.gif')
+        Prey.__init__(self, x, y, graphic_file.width(), graphic_file.height(), 0, 5)
         self.randomize_angle()
 
     def update():
+        if random() <= 0.3:
+            self.set_velocity(min(7, max(3, self._speed + random()-0.5)), self._angle + random()-0.5 )
         self.move()
 
-    def display():
-        x,y = self.get_location()
-        blue = '#0000FF'
-        canvas.create_oval(x-self.radius, y-self.radius, x+self.radius, y+self.radius,fill=blue)
+    def display(self,canvas):
+       canvas.create_image(*self.get_location(),image=self._image)
